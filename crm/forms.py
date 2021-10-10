825b45636867a54c1c6281f3be7c5e46f63e7519
David Van Vleet <56968216+davevanvleet@users.noreply.github.com>
2021-10-09 23:02:13 -0500

    Update forms.py

--- crm/forms.py
+++ crm/forms.py
@@ -16,4 +16,18 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('cust_name', 'product', 'p_description', 'quantity', 'pickup_time', 'charge', 'created_date')
+        
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+	    model = User
+	    fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+	    user = super(NewUserForm, self).save(commit=False)
+	    user.email = self.cleaned_data['email']
+	    if commit:
+		    user.save()
+	    return user
 
